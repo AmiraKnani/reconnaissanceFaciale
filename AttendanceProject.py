b4778ca567fcf9edc23c68b8f3ad58ce0dae5479
AmiraKnani <104366049+AmiraKnani@users.noreply.github.com>
2022-11-20 16:58:25 +0100

    Revert "test beard module"
    This reverts commit aded7a2a4de079f15b23ac50cf2e1b0ffd1092a9.
    
    # Conflicts:
    #	AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -27,6 +27,49 @@ print(classNames)
 
 #test
 
+def findBeard():
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    isFound= False;
+    _, img = cap.read()
+
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Gray Scale, Image factor, Number of Neighbours
+    # To construct a rectangle around the face detected
+    for (x, y, w, h) in faces:
+
+        # create a mask image of the same shape as input image, filled with 0s (black color)
+        mask = np.zeros_like(img)
+        # create a white filled ellipse
+        mask = cv2.ellipse(mask, (int((x + w) / 1.2), y + h), (69, 69), 0, 0, -180, (255, 255, 255), thickness=-1)
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+
+        # Bitwise AND operation to black out regions outside the mask
+        result = np.bitwise_and(img, mask)
+
+        # Converting the final result as HSV inorder to detect colors
+        hsv_img = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
+
+        # Draws a rectangle
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Black Color
+        low_black = np.array([94, 80, 2])
+        high_black = np.array([126, 255, 255])
+
+        MASK = cv2.inRange(hsv_img, low_black, high_black)
+
+        # If the MASK only has black pixels caused due to no black colour in the original image
+        if cv2.countNonZero(MASK) == 0:
+            print("Beard Not Found")
+        else:
+            print("Beard Found")
+            isFound = True
+    return isFound
+
 def findEncoding(images):
     encodeList = []
     for img in images :
@@ -35,6 +78,17 @@ def findEncoding(images):
         encodeList.append(encode)
     return encodeList
 
+def getRole(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.readlines(f'\n{name},{dtString}')
 def markAttendance(name):
     with open('Attendance.csv','r+') as f:
         myDataList = f.readlines()
@@ -89,9 +143,14 @@ for encodeFace, faceLoc in zip(encodesCurFrame,facesCurFrame) :
                             font=('Microsoft YaHei UI Light', 22, 'bold'))
         heading.place(x=100, y=5)
 
-        welcome = Label(frame, text='Welcome MADAME/SIR', fg='black', bg='white',
+        if(findBeard()):
+            welcome = Label(frame, text='Welcome SIR', fg='black', bg='white',
+                                font=('Microsoft YaHei UI Light', 14, 'bold'))
+            welcome.place(x=100, y=100)
+        else:
+            welcome = Label(frame, text='Welcome MADAME', fg='black', bg='white',
                             font=('Microsoft YaHei UI Light', 14, 'bold'))
-        welcome.place(x=100, y=100)
+            welcome.place(x=100, y=100)
 
         AffName = Label(frame, text=name, fg='black', bg='white',
                             font=('Bradley Hand ITC', 25,'bold'))
